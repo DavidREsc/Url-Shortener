51b51c7f0b0aab5ba7903cefcb9c3f38c169e7b8
David Rapala-Escorcia <david.rapalae@gmail.com>
2023-05-16 14:51:52 -0400

    Change path

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,23 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -17,12 +36,15 @@ const validateUrl_1 = __importDefault(require("./middleware/validateUrl"));
 const node_fetch_1 = __importDefault(require("node-fetch"));
 const cors_1 = __importDefault(require("cors"));
 const path = require('path');
+const dotenv = __importStar(require("dotenv"));
+dotenv.config();
 const app = express_1.default();
 const PORT = process.env.PORT || 3001;
 app.use(express_1.default.json());
 app.use(cors_1.default());
 if (process.env.NODE_ENV === 'production') {
-    app.use(express_1.default.static(path.join(__dirname, '../client/build/index.html')));
+    console.log('Starting in production');
+    app.use(express_1.default.static(path.join(__dirname, '../client')));
 }
 app.post('/', validateUrl_1.default, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
@@ -37,7 +59,8 @@ app.post('/', validateUrl_1.default, (req, res) => __awaiter(void 0, void 0, voi
     }
 }));
 app.get('/*', (req, res) => {
-    res.sendFile('../client/build/index.html', { root: __dirname });
+    const indexPath = path.join(__dirname, '../client/index.html');
+    res.sendFile(indexPath);
 });
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
